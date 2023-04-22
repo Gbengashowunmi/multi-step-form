6585f61e283be47ef7e6d13a9a57172393f3c169
unknown <gbengashowunmi01@gmail.com>
2023-04-22 12:33:09 +0100

    3rd commit

--- src/components/thankYou/ThankYouPage.jsx
+++ src/components/thankYou/ThankYouPage.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import thankYou from "../../assets/images/icon-thank-you.svg"
 import "./thankyou.scss"
+import FormContext from "../../contextAPI/FormContext";
 export default function ThankYouPage() {
+  const {formDetails } = useContext(FormContext);
+const name = formDetails.name.split(" ")[0];
+const planName = formDetails.plan.planName;
+
   return (
     <div className="thank_you">
       <div className="image">
@@ -9,9 +14,9 @@ export default function ThankYouPage() {
       </div>
       <h3>Thank You!</h3>
       <p>
-        Thanks for confirming your subscription! I hope you have fun trying out
+        {`${name}, thanks for confirming your subscription! You chose a ${formDetails.isMonthly?`Monthly`:`Yearly`} plan name of ${planName}. I hope you have fun trying out
         my multi step form. If you ever need support, feel free to email me at
-        gbengalilmahn@gmail.com
+        gbengalilmahn@gmail.com`}
       </p>
     </div>
   );
